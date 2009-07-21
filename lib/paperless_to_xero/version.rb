c408a4e821615c5774b6cd47e3e8f4390a110845
Matt Patterson <matt@reprocessed.org>
2009-07-21 20:40:10 +0100

    Fixed bug caused by Paperless adding commas to amounts over 1,000

--- lib/paperless_to_xero/version.rb
+++ lib/paperless_to_xero/version.rb
@@ -6,7 +6,7 @@ module PaperlessToXero
   module Version
     MAJOR = 1
     MINOR = 0
-    POINT = 2
+    POINT = 3
     FULL = [PaperlessToXero::Version::MAJOR, PaperlessToXero::Version::MINOR, PaperlessToXero::Version::POINT].join('.')
   end
 end
